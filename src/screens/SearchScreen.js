0df79ff4569dd0ea77f676aa7c1a1a1140c7ee36
dangvancuong <dangvancuong196@gmail.com>
2020-08-31 08:03:52 +0700

    Using outside api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,41 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async () => {
+        console.log('hi all')
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: term,
+                    location: 'san jose'
+                }
+            });
+
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Something went wrong')
+        }
+    }
+
+    // searchApi('pass');
+
     return (
         <View>
             <SearchBar
                 term={term}
                 onTermChange={newTerm => setTerm(newTerm)}
-                onTerSubmit={() => console.log('onTerSubmit')}
+                onTerSubmit={searchApi}
             />
-            <Text>abc</Text>
+            {errorMessage ? <Text>{ errorMessage }</Text> : null}
+            <Text>hass {results.length}</Text>
             <Text>{term}</Text>
         </View>
     );
